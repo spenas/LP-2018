cfd943fa06a6b8e3b82c5758f598d1871687a6f2
Santiago Pena <spenas@unal.edu.co>
2018-04-03 09:03:18 -0500

    Solucionado problema id, se pueden reconocer palabras especiales

--- analex/src/Main.java
+++ analex/src/Main.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 
 /*
 ERRORES ACTUALES:
--Identifica cuando hay un error lexico (un id que comienza por numero) pero sigue analizando y ahi si encuentra un id.
-Ejemplo:
-1b
->>>Error Lexico(Linea: 0, Columna: 0 )
-<id,b,0,0>
+
 
 
  */
@@ -39,51 +35,67 @@ public class Main {
         String letra[] = bloque.split("");
         String palabra_prov = "";
 
-       if(comienzaNumero(letra)){
+       if(comienzaIlegal(letra)){
             imprimir("error", fila , columna);
+            //Ta luego, no analiza mas
+            return;
         }
 
         for (int i = 0; i < letra.length ; i++){
             //#Vamos a identificar si hay un id o palabra reservada
-            if (ALFABETO.contains(letra[i])){
+            if (ALFABETO.contains(letra[i]) || letra[i].equals("_")){
                 boolean verificador = true;
                 int contador = i ;
                 while (verificador && contador < letra.length){
                     String actual = letra[contador];
-                    if (ESPECIALES.contains(actual) || OPERACION.contains(actual) ){
+                    if ((ESPECIALES.contains(actual) && !actual.equals("_")) || OPERACION.contains(actual) ){
                         break;
                     }
                     palabra_prov += actual;
                     contador++;
                 }
-                System.out.println("<id,"+palabra_prov+","+fila+","+columna+">");
+                String compReservada = esReservada(palabra_prov);
+                if(compReservada != null){
+                    System.out.println("<"+compReservada+ "," + fila + "," + columna + ">");
+                }
+                else{
+                    System.out.println("<id,"+palabra_prov+","+fila+","+columna+">");
+                }
+
                 i = contador;
             }
 
         }
     }
 
-    //funcion para verificar que un id no comience por numero
-    public static boolean comienzaNumero( String[] letra ){
+    //funcion para verificar que un id no comience por numero o por un caracter no valido para un id
+    public static boolean comienzaIlegal( String[] letra ) {
 
         //Se mira si hay un id que comience por numero
-        if (NUMERO.contains(letra[0]) ){
+        if (NUMERO.contains(letra[0])) {
             //Nos aseguramos de que en verdad se trate de un token y no de una expresion como "2+a"
-            for (int i = 1; i < letra.length ; i++){
-                if(ALFABETO.contains(letra[i])){
-                   return true;
-                }
-                else{
+            for (int i = 1; i < letra.length; i++) {
+                if (ALFABETO.contains(letra[i])) {
+                    return true;
+                } else {
                     //Nos aseguramos de que se permita hacer por ejemplo: "2+a"
-                    if (OPERACION.contains(letra[i])){
+                    if (OPERACION.contains(letra[i])) {
                         return false;
                     }
                 }
             }
 
+        } else if (OPERACION.contains(letra[0]) || ESPECIALES.contains(letra[0]) && !letra[0].equals("_")) {
+            for (int i = 1; i < letra.length; i++) {
+                if (ALFABETO.contains(letra[i])) {
+                    return true;
+                }
+
+            }
         }
-        return false;
-    }
+            return false;
+        }
+
 
     //Se verifica si el caracter es algun simbolo aritmetico o corchetes, parentesis etc
     public static boolean esSimbolo( String texto ){
@@ -92,8 +104,37 @@ public class Main {
     }
 
     //Se verifica si el token es palabra reservada
-    public static boolean esReservada( String texto ){
-        return  false;
+    public static String esReservada( String texto ){
+        switch (texto){
+            case "log":
+                return "log";
+            case "end":
+                return "end";
+            case "while":
+                return "while";
+            case "funcion":
+                return "funcion";
+            case "retorno":
+                return "retorno";
+            case "for":
+                return "for";
+            case "in":
+                return "in";
+            case "fabs":
+                return "fabs";
+            case "pow":
+                return "pow";
+            case "sqrt":
+                return "sqrt";
+            case "cos":
+                return "cos";
+            case "sin":
+                return "sin";
+
+
+        }
+
+        return  null;
     }
     public static void imprimir (String token, int fila , int colum){
         if (token.equals("error")){
